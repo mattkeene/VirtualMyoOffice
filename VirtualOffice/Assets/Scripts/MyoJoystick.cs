25493f27d94be9e7326d191f0be60e3ea387d735
mattkeene <mkeene08@gmail.com>
2015-09-05 19:15:41 -0400

    First commit: Oculus and Myo both integrated in their own scenes. Addition script created adding offset calculation functionality in preparation for movement control via Myo.

---
+++ VirtualOffice/Assets/Scripts/MyoJoystick.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MyoJoystick : MonoBehaviour {
+
+	private Vector3 origin;
+
+	public Vector3 offset = new Vector3(0,0,0);
+
+	private bool hasPinned = false;
+
+	// Use this for initialization
+	void Start () {
+		origin = this.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (hasPinned) offset = this.transform.position - origin;
+	}
+
+	public void PinOrigin () {
+		hasPinned = true;
+		origin = this.transform.position;
+	}
+
+}
